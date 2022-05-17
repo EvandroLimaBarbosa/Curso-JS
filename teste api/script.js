02c4fdfc7b740716a5f2e5175911a9b7ef57d4fa
Evandro Lima Barbosa <evandrolimabarbosa@hotmail.com>
2022-05-16 21:45:02 -0300

    aprendendo api

---
+++ teste api/script.js
@@ -0,0 +1,76 @@
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('users');
+const url = 'https://6282db2492a6a5e4621a86f3.mockapi.io/api/start/usuario';
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+    
+  let authors = data;
+  return authors.map(function(author) {
+      console.log(author)
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('p');
+    let data = createNode('p');
+    img.src = author.avatar;
+    span.innerHTML = `${author.name}`;
+    data.innerHTML = ` ${author.createdAt}`
+
+    let txtdata = `${author.createdAt}`;
+
+    append(li, img);
+    append(li, span);
+    append(li, data);
+    append(ul, li);
+    console.log(authors)
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
+
+
+
+/*
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('users');
+const url = 'https://randomuser.me/api/?results=10';
+
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = data.results;
+  return authors.map(function(author) {
+    
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('span');
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
+
+*/
