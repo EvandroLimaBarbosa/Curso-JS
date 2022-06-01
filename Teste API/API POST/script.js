7e2b44b6b789ec3ac3f65ed14e289e023beee376
Evandro Lima Barbosa <evandrolimabarbosa@hotmail.com>
2022-05-31 23:33:06 -0300

    ATT

--- Teste API/API POST/script.js
+++ Teste API/API POST/script.js
@@ -12,3 +12,51 @@ btn.addEventListener('click', ()=>{
 
 }
 })
+
+
+function entrar(){
+    
+    let usuario = document.getElementById('usuario');
+    let userLabel = document.getElementById('userLabel');
+    
+    let senha = document.getElementById('senha');
+    let senhaLabel = document.getElementById('senhaLabel');
+
+    
+    let msgError = document.getElementById('msgError');
+
+    let listaUser = []
+
+    let userValid = {
+        nome: '',
+        user: '',
+        senha: ''
+    }
+
+    listaUser = JSON.parse(localStorage.getItem('listaUser'));
+
+    listaUser.forEach((item) =>{
+        if(usuario.value ==  item.userCad && senha.value == item.senhaCad){
+            userValid = {
+                nome: item.nomeCad,
+                user: item.userCad,
+                senha: item.senhaCad
+            }
+        }
+    })
+
+    if(usuario.value == userValid.user && senha.value == userValid.senha){
+        alert('Deu Certo')
+    }else {
+        userLabel.setAttribute('style', 'color: red');
+        usuario.setAttribute('style', 'border-color: red');
+        senhaLabel.setAttribute('style', 'color: red');
+        senha.setAttribute('style', 'border-color: red');
+
+        msgError.setAttribute('style', 'display: block')
+        msgError.innerHTML = 'Usuario ou senha incorretos'
+
+        usuario.focus()
+    }
+
+}
